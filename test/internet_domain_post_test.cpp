0565e3f5697c49dd175f7dad44bf353189c943a4
harsathAI <harsath@tuta.io>
2020-09-14 19:05:32 -0400

    Test helper file: refactor

--- test/internet_domain_post_test.cpp
+++ test/internet_domain_post_test.cpp
@@ -1,36 +1,7 @@
 #include "../internet_domain_http.cpp"
+#include "./test_helper.cpp"
 #include <cstdlib>
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define BOOL(arg) (arg) ? "true" : "false"
-
-template<typename T>
-inline void print(T arg){
-	std::cout << arg << std::endl;
-}
-template<typename T, typename... Targs>
-inline void print(T arg, Targs... args){
-	print(arg);
-	print(args...);
-}
-
-std::size_t _total_test_count = 0;
-std::size_t _fail_count = 0;
-std::size_t _ok_cout = 0;
-
-template<typename T1, typename T2>
-static inline void ASSERT_EQ(const T1& value_one, const T2& value_two, const std::string& print_console){
-	_total_test_count += 1; 
-	if(value_one == value_two){
-		std::cout << GREEN << "[ OK ] -> " << RESET << print_console << "\n";
-		_ok_cout += 1;
-	}else{
-		std::cout << RED << "[ FAILED ] -> " << RESET << print_console << " " << "Value returned: " << value_one << "\n";
-		_fail_count += 1;
-	}
-}
 
 class stream_sock_test{
 private:
@@ -99,7 +70,6 @@ int main(int argc, char **argv){
 	test.x_www_form_urlencoded_parset_test();
 	test.private_member_initilization_checks();
 
-
-	std::cout << "\n\nRESULTS:- (Total Tests: " << _total_test_count << ") \n" << GREEN << "Total OK: " << _ok_cout << RESET << "\n" << RED << "Total FAILED: " << _fail_count << RESET << std::endl;
+	print_final_stat();
 }
 
---
+++ test/test_helper.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define BOOL(arg) (arg) ? "true" : "false"
+
+template<typename T>
+inline void print(T arg){
+	std::cout << arg << std::endl;
+}
+template<typename T, typename... Targs>
+inline void print(T arg, Targs... args){
+	print(arg);
+	print(args...);
+}
+
+static std::size_t _total_test_count = 0;
+static std::size_t _fail_count = 0;
+static std::size_t _ok_cout = 0;
+
+template<typename T1, typename T2>
+static inline void ASSERT_EQ(const T1& value_one, const T2& value_two, const std::string& print_console){
+	_total_test_count += 1; 
+	if(value_one == value_two){
+		std::cout << GREEN << "[ OK ] -> " << RESET << print_console << "\n";
+		_ok_cout += 1;
+	}else{
+		std::cout << RED << "[ FAILED ] -> " << RESET << print_console << " " << "Value returned: " << value_one << "\n";
+		_fail_count += 1;
+	}
+}
+
+static inline void print_final_stat(void){
+	std::cout << "\n\nRESULTS:- (Total Tests: " << _total_test_count << ") \n" << GREEN << "Total OK: " << _ok_cout << RESET << "\n" << RED << "Total FAILED: " << _fail_count << RESET << std::endl;
+}
